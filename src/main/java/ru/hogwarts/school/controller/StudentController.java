a783148716e9137fdb81be8a1e02add484f85503
MorozovVladislavSergeevich <vsmorozov@edu.hse.ru>
2025-01-08 20:30:18 +0300

    добавил crud-эндпоинты в StudentController

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,10 +1,49 @@
 package ru.hogwarts.school.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
 
 @RestController
 @RequestMapping("student")
 public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Student> getStudentInfo(@PathVariable Long id) {
+        Student student = studentService.getStudentById(id);
+        if (student == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student){
+        return studentService.createStudent(student);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Student> editFaculty(@RequestBody Student student,@PathVariable Long id){
+        Student editedStudent = studentService.editStudent(id,student);
+        if(editedStudent == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(editedStudent);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Student> deleteFaculty(@PathVariable Long id){
+        Student deletedStudent = studentService.deleteStudent(id);
+        if(deletedStudent == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(deletedStudent);
+    }
 
 }
