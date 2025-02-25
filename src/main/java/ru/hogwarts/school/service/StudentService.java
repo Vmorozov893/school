a0853fe33e8cf5be1673e4622e0425cd89562eda
MorozovVladislavSergeevich <vsmorozov@edu.hse.ru>
2025-02-25 22:11:01 +0300

    готовое дз

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -3,45 +3,41 @@ package ru.hogwarts.school.service;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.StudentRepository;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
 
-    private Long count = 0L;
+    private final StudentRepository studentRepository;
 
-    HashMap<Long, Student> studentsHashMap = new HashMap<>();
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
-    public Student createStudent(Student student){
-        studentsHashMap.put(++count,student);
-        student.setId(count);
-        return student;
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
     }
 
     public Student getStudentById(Long id) {
-        return studentsHashMap.get(id);
+        return studentRepository.findById(id).orElse(null);
     }
 
     public Student editStudent(Long id, Student student) {
-        if(!studentsHashMap.containsKey(id)){
-            return null;
-        }
-        student.setId(id);
-        studentsHashMap.put(id, student);
-        return student;
+        Student student1 = studentRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+        student1.setName(student.getName());
+        student1.setAge(student.getAge());
+        return studentRepository.save(student1);
     }
 
-    public Student deleteStudent(Long id) {
-        return studentsHashMap.remove(id);
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
     }
 
-    public List<Student> filterForAge(int age){
-        return studentsHashMap.values().stream().filter(e->e.getAge()==age).collect(Collectors.toList());
+    public List<Student> filterForAge(int age) {
+        return studentRepository.findAll().stream().filter(e -> e.getAge() == age).collect(Collectors.toList());
 
     }
 
