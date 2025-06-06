cedf324b72c800901d1a245b7ef6d258ab47fd0f
MorozovVladislavSergeevich <vsmorozov@edu.hse.ru>
2025-06-06 16:20:27 +0300

    StudentControllerTestRestTemplate ready

--- src/test/java/ru/hogwarts/school/controller/StudentControllerTestRestTemplate.java
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerTestRestTemplate.java
@@ -1,7 +1,5 @@
 package ru.hogwarts.school.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +8,11 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.FacultyRepository;
 import ru.hogwarts.school.repository.StudentRepository;
 
-import java.util.Collection;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,11 +26,13 @@ public class StudentControllerTestRestTemplate {
     private int port;
 
     @Autowired
-    StudentRepository repository;
+    StudentRepository studentRepository;
+    @Autowired
+    FacultyRepository facultyRepository;
 
     @BeforeEach
     public void clearDatabase() {
-        repository.deleteAll();
+        studentRepository.deleteAll();
     }
 
     @Test
@@ -59,7 +58,7 @@ public class StudentControllerTestRestTemplate {
     @Test
     void shouldGetStudentInfo() {
         Student student = new Student("Garry Potter", 11);
-        student = repository.save(student);
+        student = studentRepository.save(student);
 
         ResponseEntity<Student> studentResponseEntity = restTemplate.getForEntity(
                 "http://localhost:" + port + "/student/" + student.getId(),
@@ -78,7 +77,7 @@ public class StudentControllerTestRestTemplate {
     @Test
     void shouldEditStudent() {
         Student student = new Student("Garry Potter", 11);
-        student = repository.save(student);
+        student = studentRepository.save(student);
 
         Student newStudent = new Student("Ron Weasley", 13);
 
@@ -103,7 +102,7 @@ public class StudentControllerTestRestTemplate {
     @Test
     void shouldDeleteStudent() {
         Student student = new Student("Garry Potter", 11);
-        student = repository.save(student);
+        student = studentRepository.save(student);
 
         ResponseEntity<Void> studentResponseEntity = restTemplate.exchange(
                 "http://localhost:" + port + "/student/" + student.getId(),
@@ -115,14 +114,14 @@ public class StudentControllerTestRestTemplate {
     }
 
     @Test
-    void shouldStudentsByAge(){
-        int age=12;
-        Student student1 = new Student("Garry Potter", age-1);
+    void shouldStudentsByAge() {
+        int age = 12;
+        Student student1 = new Student("Garry Potter", age - 1);
         Student student2 = new Student("Ron Weasley", age);
         Student student3 = new Student("Hermione Granger", age);
-        student1 = repository.save(student1);
-        student2 = repository.save(student2);
-        student3 = repository.save(student3);
+        student1 = studentRepository.save(student1);
+        student2 = studentRepository.save(student2);
+        student3 = studentRepository.save(student3);
         final Long id1 = student1.getId();
         final Long id2 = student2.getId();
         final Long id3 = student3.getId();
@@ -132,7 +131,8 @@ public class StudentControllerTestRestTemplate {
                 "http://localhost:" + port + "/student?age=" + age,
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<List<Student>>(){}
+                new ParameterizedTypeReference<List<Student>>() {
+                }
         );
 
         assertNotNull(studentResponseEntity);
@@ -140,9 +140,9 @@ public class StudentControllerTestRestTemplate {
 
         List<Student> actualStudents = studentResponseEntity.getBody();
         assertEquals(2, actualStudents.size());
-        Student actualStudent1 = actualStudents.stream().filter(e->e.getId().equals(id1)).findFirst().orElse(null);
-        Student actualStudent2 = actualStudents.stream().filter(e->e.getId().equals(id2)).findFirst().orElse(null);
-        Student actualStudent3 = actualStudents.stream().filter(e->e.getId().equals(id3)).findFirst().orElse(null);
+        Student actualStudent1 = actualStudents.stream().filter(e -> e.getId().equals(id1)).findFirst().orElse(null);
+        Student actualStudent2 = actualStudents.stream().filter(e -> e.getId().equals(id2)).findFirst().orElse(null);
+        Student actualStudent3 = actualStudents.stream().filter(e -> e.getId().equals(id3)).findFirst().orElse(null);
 
         assertNull(actualStudent1);
         assertNotNull(actualStudent2);
@@ -159,10 +159,66 @@ public class StudentControllerTestRestTemplate {
 
     @Test
     void shouldFindByAgeBetween() {
+        int age = 12;
+        Student student1 = new Student("Garry Potter", age - 1);
+        Student student2 = new Student("Ron Weasley", age);
+        Student student3 = new Student("Hermione Granger", age + 1);
+        student1 = studentRepository.save(student1);
+        student2 = studentRepository.save(student2);
+        student3 = studentRepository.save(student3);
+        final Long id1 = student1.getId();
+        final Long id2 = student2.getId();
+        final Long id3 = student3.getId();
+
+
+        ResponseEntity<List<Student>> studentResponseEntity = restTemplate.exchange(
+                "http://localhost:" + port + "/student/findByAgeBetween?min=" + (age - 1) + "&max=" + (age),
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Student>>() {
+                }
+        );
+
+        assertNotNull(studentResponseEntity);
+        assertEquals(studentResponseEntity.getStatusCode(), HttpStatusCode.valueOf(200));
+
+        List<Student> actualStudents = studentResponseEntity.getBody();
+        assertEquals(2, actualStudents.size());
+        Student actualStudent1 = actualStudents.stream().filter(e -> e.getId().equals(id1)).findFirst().orElse(null);
+        Student actualStudent2 = actualStudents.stream().filter(e -> e.getId().equals(id2)).findFirst().orElse(null);
+        Student actualStudent3 = actualStudents.stream().filter(e -> e.getId().equals(id3)).findFirst().orElse(null);
+
+        assertNull(actualStudent3);
+        assertNotNull(actualStudent1);
+        assertNotNull(actualStudent2);
+        assertEquals(actualStudent1.getId(), student1.getId());
+        assertEquals(actualStudent1.getName(), student1.getName());
+        assertEquals(actualStudent1.getAge(), student1.getAge());
+        assertEquals(actualStudent2.getId(), student2.getId());
+        assertEquals(actualStudent2.getName(), student2.getName());
+        assertEquals(actualStudent2.getAge(), student2.getAge());
 
     }
+
     @Test
-    void shouldFacultyByStudent(){
+    void shouldFacultyByStudent() {
+        Faculty faculty = new Faculty("Gryffindor", "brown");
+        faculty = facultyRepository.save(faculty);
+        Student student = new Student("Garry Potter", 12, faculty);
+        student = studentRepository.save(student);
+
+        ResponseEntity<Faculty> facultyResponseEntity = restTemplate.getForEntity(
+                "http://localhost:" + port + "/student/facultyByStudent?id=" +student.getId(),
+                Faculty.class
+        );
+
+        assertNotNull(facultyResponseEntity);
+        assertEquals(facultyResponseEntity.getStatusCode(), HttpStatusCode.valueOf(200));
+
+        Faculty actualFaculty = facultyResponseEntity.getBody();
+        assertEquals(actualFaculty.getId(), faculty.getId());
+        assertEquals(actualFaculty.getName(), faculty.getName());
+        assertEquals(actualFaculty.getColor(), faculty.getColor());
 
     }
 
