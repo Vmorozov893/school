49c609c6c7e1681b9493894445528c91ec247275
MorozovVladislavSergeevich <vsmorozov@edu.hse.ru>
2025-06-09 05:27:12 +0300

    Avatar @Post

--- src/main/java/ru/hogwarts/school/controller/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controller/AvatarController.java
@@ -1,15 +1,25 @@
 package ru.hogwarts.school.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.service.AvatarService;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("avatar")
 public class AvatarController {
-    private final AvatarService service;
+    private final AvatarService avatarService;
 
-    public AvatarController(AvatarService service) {
-        this.service = service;
+    public AvatarController(AvatarService avatarService) {
+        this.avatarService = avatarService;
+    }
+
+    @PostMapping(value = "/{studentId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar(@PathVariable Long studentId, @RequestParam MultipartFile avatar) throws IOException {
+        avatarService.uploadAvatar(studentId, avatar);
+        return ResponseEntity.ok().build();
     }
 }
