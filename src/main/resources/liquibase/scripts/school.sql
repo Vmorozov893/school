55b71b4bdcb97851de5e2e484472c00a0e342899
MorozovVladislavSergeevich <vsmorozov@edu.hse.ru>
2025-06-14 19:45:12 +0300

    добавил исправления

--- src/main/resources/liquibase/scripts/school.sql
+++ src/main/resources/liquibase/scripts/school.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+
+--changeset author:MorozovVladislavSergeevich
 CREATE INDEX idx_student_name ON student (name);
 
 CREATE INDEX idx_faculty_name_color ON faculty (name, color);
